5d9ef245dbf2c5fd3f623ef5518aa5ac213dacbc
josepaltadill <josepaltadill@hotmail.com>
2022-08-19 10:32:53 +0200

    sync

--- src/components/container/figura.jsx
+++ src/components/container/figura.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Figura = () => {
+    const [buttonStyle, setButtonStyle] = useState('black');
+    function entraRaton() {
+        console.log('entra raton');
+    }
+
+    function saleRaton() {
+        console.log('sale raton');
+    }
+
     return (
-        <div>
-            Figura
-        </div>
+        <div style={{width:'255px', height:'255px', backgroundColor:'black'}} onMouseEnter={entraRaton} onMouseLeave={saleRaton}></div>
     );
 }
 
