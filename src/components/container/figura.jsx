262003436c5de17e947b754329cc7d6e3c99c192
josepaltadill <josepaltadill@hotmail.com>
2022-08-23 19:44:14 +0200

    sync

--- src/components/container/figura.jsx
+++ src/components/container/figura.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Figura = () => {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    let interval;
+
     const [buttonStyle, setButtonStyle] = useState('black');
+    const divStyle = {
+        width:'255px', 
+        height:'255px',
+        backgroundColor: buttonStyle,
+    }
+    function console() {
+        //console.log('entra raton');
+        setButtonStyle(color);
+    }
     function entraRaton() {
-        console.log('entra raton');
+        //console.log('entra raton');
+        //setButtonStyle(color);
+        //setInterval(setButtonStyle(color), 1000);
+        interval = setInterval(console, 1000);
     }
 
     function saleRaton() {
-        console.log('sale raton');
+        //console.log('sale raton');
+        //setButtonStyle(buttonStyle);
+        clearInterval(interval);
     }
 
     return (
-        <div style={{width:'255px', height:'255px', backgroundColor:'black'}} onMouseEnter={entraRaton} onMouseLeave={saleRaton}></div>
+        <div style={divStyle} onMouseEnter={entraRaton} onMouseLeave={saleRaton}></div>
     );
 }
 
