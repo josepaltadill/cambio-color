a480b97a8ea2cc1ad8a33a30698dcce28f2d7cb8
josepaltadill <josepaltadill@hotmail.com>
2022-08-24 20:17:56 +0200

    exercici acabat

--- src/components/container/figura.jsx
+++ src/components/container/figura.jsx
@@ -1,37 +1,47 @@
 import React, {useState} from 'react';
 
-const Figura = () => {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
-    let color = `rgb(${red}, ${green}, ${blue})`;
-    let interval;
+const getRandomColor = () => {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
 
+const Figura = () => {
     const [buttonStyle, setButtonStyle] = useState('black');
+    const [intervals, setIntervals] = useState(null);
     const divStyle = {
         width:'255px', 
         height:'255px',
         backgroundColor: buttonStyle,
     }
-    function console() {
-        //console.log('entra raton');
-        setButtonStyle(color);
-    }
-    function entraRaton() {
-        //console.log('entra raton');
-        //setButtonStyle(color);
-        //setInterval(setButtonStyle(color), 1000);
-        interval = setInterval(console, 1000);
+
+    const setBackgroundColor = () => {
+        setButtonStyle(getRandomColor());
     }
 
-    function saleRaton() {
-        //console.log('sale raton');
-        //setButtonStyle(buttonStyle);
-        clearInterval(interval);
+    const entraRaton = () => {
+        setIntervals (setInterval(() => {
+            setBackgroundColor();
+        }, 1000));
+    }
+
+    const saleRaton = () => {
+        if(intervals) {
+            clearInterval(intervals);
+            setIntervals(null);
+        }
+    }
+
+    const dobleClick = () => {
+        if(intervals) {
+            clearInterval(intervals);
+            setIntervals(null);
+        }
     }
 
     return (
-        <div style={divStyle} onMouseEnter={entraRaton} onMouseLeave={saleRaton}></div>
+        <div style={divStyle} onMouseEnter={entraRaton} onMouseLeave={saleRaton} onDoubleClick={dobleClick}></div>
     );
 }
 
